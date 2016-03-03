55f644e1931ee6eb427d3c037573c0ccb7fdf921
d_kujiraoka <d_kujiraoka@bbmedia.co.jp>
2016-03-03 19:28:12 +0900

    js

--- js/response.js
+++ js/response.js
@@ -4,14 +4,14 @@ jQuery(function($) {
         //PC環境の場合
         if (window.matchMedia( '(min-width: 769px)' ).matches) {　//切り替える画面サイズ
             $.ajax({
-                url: '/kimagure-hp/wp-content/themes/kimagure/js/app.js',
+                url: '/kimagure/wp-content/themes/kimagure/js/app.js',
                 dataType: 'script',
                 cache: false
            });
         //モバイル環境の場合
         } else {
             $.ajax({
-                url: '/kimagure-hp/wp-content/themes/kimagure/js/sp.js',
+                url: '/kimagure/wp-content/themes/kimagure/js/sp.js',
                 dataType: 'script',
                 cache: false
             });
